87da12526cbd0d37fb7d1859f24d282d39d3fbbf
getabetterpic <dan@callrail.com>
2019-12-04 07:04:44 -0500

    Update author info

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,11 +1,11 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Starter Blog`,
-    author: `Kyle Mathews`,
-    description: `A starter blog demonstrating what Gatsby can do.`,
-    siteUrl: `https://gatsby-starter-blog-demo.netlify.com/`,
+    title: `Rocket Musings`,
+    author: `Dan Smith`,
+    description: `Random Thoughts about Rockets.`,
+    siteUrl: `https://rockets.getabetterpic.com/`,
     social: {
-      twitter: `kylemathews`,
+      twitter: `getabetterpic`,
     },
   },
   plugins: [
